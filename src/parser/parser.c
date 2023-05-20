c806bd5a6969dd4e84445199b6f2be68bd65aea8
chanhihi <chanhihi55@gmail.com>
2023-05-21 04:28:11 +0900

    wip(validator): staging map validator

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,6 +1,6 @@
 #include "parse.h"
 
-void	init_info(t_info *info)
+void	initialize_info(t_info *info)
 {
 	info->n_texpath = ((void *)0);
 	info->s_texpath = ((void *)0);
@@ -27,20 +27,66 @@ int	file_open(char *path)
 	return (fd);
 }
 
+int	staging_map_checker(t_info *info)
+{
+	if (info->n_texpath == 0)
+		return (0);
+	if (info->s_texpath == 0)
+		return (0);
+	if (info->w_texpath == 0)
+		return (0);
+	if (info->e_texpath == 0)
+		return (0);
+	if (info->floor_color == -1)
+		return (0);
+	if (info->ceiling_color == -1)
+		return (0);
+	return (1);
+}
+
+int	is_just_new_line(char *line)
+{
+	if (line[0] == '\n')
+		return (1);
+	return (0);
+}
+
+void	fill_in_map_information(t_info *info)
+{
+	// strcmp info ~
+}
+
+void	map_parsing_validator(int fd, t_info *info)
+{
+	char	*line;
+
+	while (!staging_map_checker(info))
+	{
+		line = get_next_line(fd);
+		if (line == (void *)0)
+			exit_with_error("Important map information is missing.");
+		else if (is_just_new_line(line))
+		{
+			free(line);
+			continue ;
+		}
+		else
+			fill_in_map_information(info);
+		free(line);
+	}
+}
+
+
 void	parser(t_info *info, char *path)
 {
 	int	fd;
 
-	// file open
 	fd = file_open(path);
-	// init
-	init_info(info);
-	// map parsing validator
-
+	initialize_info(info);
+	map_parsing_validator(fd, info);
 	// map validator
 
 	// texture setting
 
-	// file close
 	close (fd);
 }
