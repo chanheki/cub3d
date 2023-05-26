5827382048a539841be4ec1e7c7d3c5ff5a742d8
chanhihi <chanhihi55@gmail.com>
2023-05-26 21:38:50 +0900

    test(leaks): add test code

--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 
 #include "cub3d.h"
 
+void leaks() {
+	system("leaks cub3D");
+}
+
 int	main(int ac, char **av)
 {
 	t_info	info;
@@ -8,5 +12,6 @@ int	main(int ac, char **av)
 	program_validator(ac, av);
 	parser(&info, av[1]);
 	executor(&info);
+	atexit(leaks);
 	return (0);
 }
