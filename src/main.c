ddbb1870749c50d6abc4ffa8ab3a9bc9fa5629bd
chanhihi <chanhihi55@gmail.com>
2023-05-28 19:58:47 +0900

    chore(norm): fix for norm

--- src/main.c
+++ src/main.c
@@ -6,16 +6,11 @@
 /*   By: chanheki <chanheki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 16:32:17 by chanheki          #+#    #+#             */
-/*   Updated: 2023/05/28 16:32:30 by chanheki         ###   ########.fr       */
+/*   Updated: 2023/05/28 19:33:27 by chanheki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "cub3d.h"
-
-void leaks() {
-	system("leaks cub3D");
-}
 
 int	main(int ac, char **av)
 {
@@ -24,6 +19,5 @@ int	main(int ac, char **av)
 	program_validator(ac, av);
 	parser(&info, av[1]);
 	executor(&info);
-	atexit(leaks);
 	return (0);
 }
